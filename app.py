ad8f0242cc91f131381d5362f4470549d92f8e01
AnhTuyet Vo <voanht@flip1.engr.oregonstate.edu>
2023-01-27 12:33:26 -0800

    Added tooltip icon

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ import os
 from flask import Flask, render_template
 
 # Starting the webapp:
+# source ./venv/bin/activate
 # export FLASK_APP=app.py
 # python -m flask run -h 0.0.0.0 -p 9101 --reload
 
-# Configuration
+# Configures the class
 app = Flask(__name__)
 
-# Provide a route where requests on the web application can be addressed
+# Provides a route where requests on the web application can be addressed
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -18,7 +19,7 @@ def home():
 def search_recipe():
     return render_template("search.html")
 
-# Listener
+# Listener with port number
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 9102))
     webapp.run(port=port, debug=True)
