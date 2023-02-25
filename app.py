7dcbed19447732358b9f193cd9b81056520f5731
AnhTuyet Vo <voanht@flip1.engr.oregonstate.edu>
2023-02-24 16:14:50 -0800

    Integrated recipe microservice using server-client socket

--- app.py
+++ app.py
@@ -1,17 +1,42 @@
 from flask import Flask
-import os
 from flask import Flask, render_template
 from flask import request
+import os
+import json
+import socket
 
 # Starting the webapp:
 # source ./venv/bin/activate
 # export FLASK_APP=app.py
 # python -m flask run -h 0.0.0.0 -p 9101 --reload
 
-# Configures the class
+# Configures the flask app
 app = Flask(__name__)
 
-# Provides a route where requests on the web application can be addressed
+# Edamam API client
+def recipe_client(query):
+    # Sets up socket to send request
+    HOST = "127.0.0.1"
+    PORT = 5001
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((HOST, PORT))
+    client_socket.send(query.encode())
+
+    # Receives all packets sent by server
+    data = ""
+    while True:
+        temp = client_socket.recv(1024).decode()
+        if len(temp) == 0:
+            break
+        data += temp
+    
+    # Converts string to list object
+    data_list = json.loads(data)
+
+    client_socket.close()
+    return data_list
+
+# Provides a route where requests on the web app can be addressed
 @app.route('/')
 def home():
     return render_template("home.html")
@@ -24,13 +49,11 @@ def search_recipe():
 def search_result():
     if request.method == "POST":
         category = request.form.get("input-category")
+
+    # Starts client socket with user input
+    result = recipe_client(category)
     
-    if not category:
-        category = "not working"
-
-    return render_template("search-result.html", category=category)
-
-    # return render_template("search-result.html")
+    return render_template("search-result.html", recipes=result)
 
 @app.route('/random')
 def random_recipe():
@@ -40,7 +63,7 @@ def random_recipe():
 def about():
     return render_template("about.html")
 
-# Listener with port number
+# Listener
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 9102))
+    port = int(os.environ.get('PORT', 9101))
     webapp.run(port=port, debug=True)
